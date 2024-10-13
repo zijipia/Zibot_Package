754c2eec44fed647e23ac344c88f87fe833f1e2b
Ziji <Zijipia@gmail.com>
2024-10-13 18:50:22 +0700

    update package and zihooks

--- Zihooks/index.d.ts
+++ Zihooks/index.d.ts
@@ -1,11 +1,11 @@
-declare module "Zihooks/index" {
-	type FunctionsType = any;
-	type CommandsType = any;
-	type CooldownsType = any;
-	type ClientType = any;
+type FunctionsType = any;
+type CommandsType = any;
+type CooldownsType = any;
+type ClientType = any;
 
-	export const useFunctions: (Functions?: FunctionsType) => FunctionsType;
-	export const useCommands: (Commands?: CommandsType) => CommandsType;
-	export const useCooldowns: (Cooldowns?: CooldownsType) => CooldownsType;
-	export const useClient: (Client?: ClientType) => ClientType;
-}
+const useFunctions: (Functions?: FunctionsType) => FunctionsType;
+const useCommands: (Commands?: CommandsType) => CommandsType;
+const useCooldowns: (Cooldowns?: CooldownsType) => CooldownsType;
+const useClient: (Client?: ClientType) => ClientType;
+
+export = { useFunctions, useCommands, useCooldowns, useClient };
